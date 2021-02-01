879ff1a2123ec6d9bd4348eaea51f89e64ea9eee
cpt at mac <pantaochen@gmail.com>
2021-02-01 22:37:16 +0800

    CountDownLatch 使用示例

---
+++ src/main/java/top/dpdaidai/architect/multithreading/T25_CountDownLatch.java
@@ -0,0 +1,49 @@
+package top.dpdaidai.architect.multithreading;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 转载 https://www.cnblogs.com/dolphin0520/p/3920397.html
+ *
+ * 　CountDownLatch类位于java.util.concurrent包下，利用它可以实现类似计数器的功能。比如有一个任务A，它要等待其他4个任务执行完毕之后才能执行，此时就可以利用CountDownLatch来实现这种功能了。
+ *
+ * @Author chenpantao
+ * @Date 2/1/21 9:17 PM
+ * @Version 1.0
+ */
+public class T25_CountDownLatch {
+    public static void main(String[] args) {
+        final CountDownLatch latch = new CountDownLatch(2);
+
+        new Thread(() -> {
+            try {
+                System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
+                Thread.sleep(3000);
+                System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
+                latch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
+                Thread.sleep(3000);
+                System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
+                latch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        try {
+            System.out.println("等待2个子线程执行完毕...");
+            latch.await();
+            System.out.println("2个子线程已经执行完毕");
+            System.out.println("继续执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
