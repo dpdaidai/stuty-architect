721fd14043583505c31672cfd73cacdd08a33cc4
cpt at mac <pantaochen@gmail.com>
2021-03-18 11:59:36 +0800

    项目修改为web服务

---
+++ src/main/java/top/dpdaidai/architect/ci/TestController.java
@@ -0,0 +1,22 @@
+package top.dpdaidai.architect.ci;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author chenpantao
+ * @Date 3/18/21 11:52 AM
+ * @Version 1.0
+ */
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+
+
+}
