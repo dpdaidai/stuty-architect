64907b77ef5d261e6ff253c9cf2935fcd7bdffe4
cpt at mac <pantaochen@gmail.com>
2021-03-22 23:26:47 +0800

    增加quartz注释

--- src/main/java/top/dpdaidai/architect/quartz/MyScheduler.java
+++ src/main/java/top/dpdaidai/architect/quartz/MyScheduler.java
@@ -9,6 +9,38 @@ import java.util.concurrent.TimeUnit;
 /**
  * 转载
  * https://blog.csdn.net/noaman_wgs/article/details/80984873
+ * (1) Job和JobDetail
+ * JobDetail用来绑定Job，为Job实例提供许多属性：
+ *      name
+ *      group
+ *      jobClass
+ *      jobDataMap
+ * JobDetail绑定指定的Job，每次Scheduler调度执行一个Job的时候，首先会拿到对应的Job，
+ * 然后创建该Job实例，再去执行Job中的execute()的内容，任务执行结束后，关联的Job对象实例会被释放，且会被JVM GC清除。
+ * 为什么设计成JobDetail + Job，不直接使用Job
+ *      JobDetail定义的是任务数据，而真正的执行逻辑是在Job中。
+ *      这是因为任务是有可能并发执行，如果Scheduler直接使用Job，就会存在对同一个Job实例并发访问的问题。
+ *      而JobDetail & Job 方式，Sheduler每次执行，都会根据JobDetail创建一个新的Job实例，这样就可以规避并发访问的问题。
+ *
+ * (2)JobExecutionContext
+ *      JobExecutionContext中包含了Quartz运行时的环境以及Job本身的详细数据信息。
+ *      当Schedule调度执行一个Job的时候，就会将JobExecutionContext传递给该Job的execute()中，
+ *      Job就可以通过JobExecutionContext对象获取信息。
+ *
+ * (3)JobDataMap
+ *      JobDataMap实现了JDK的Map接口，可以以Key-Value的形式存储数据。
+ *      JobDetail、Trigger都可以使用JobDataMap来设置一些参数或信息，
+ *      Job执行execute()方法的时候，JobExecutionContext可以获取到JobExecutionContext中的信息：
+ *
+ * (4)Trigger
+ *      Trigger是Quartz的触发器，会去通知Scheduler何时去执行对应Job。
+ *          new Trigger().startAt(): 表示触发器首次被触发的时间;
+ *          new Trigger().endAt(): 表示触发器结束触发的时间;
+ *      SimpleTrigger
+ *          SimpleTrigger可以实现在一个指定时间段内执行一次作业任务或一个时间段内多次执行作业任务。
+ *      CronTrigger
+ *          CronTrigger功能非常强大，是基于日历的作业调度，而SimpleTrigger是精准指定间隔，
+ *          所以相比SimpleTrigger，CroTrigger更加常用。CroTrigger是基于Cron表达式的
  *
  * @Author chenpantao
  * @Date 3/22/21 11:18 AM
